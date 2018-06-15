898954dde7c4f6912b1d200f7ad9a840f2c21be0
yuanzy <yzy216@qq.com>
2018-06-15 14:50:03 +0800

    sync

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(Devconfig)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+     id = db.Column(db.Interger(),primary_key=True)
+     username = db.Column(db.String(250))
+     password = db.Column(db.String(250))
+
+     def __init__(self,username):
+         self.username = username
+
+     def __repr__(self):
+         return "<User '{}'".format(self.username)
+
+
+
